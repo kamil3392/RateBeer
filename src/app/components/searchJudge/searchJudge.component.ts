07298573e6eefc145d38d576201e342649e8ba66
akadzik <arkosky.qa@gmail.com>
2019-06-14 23:39:33 +0200

    Add menu to searchJudge and searchBeer

--- src/app/components/searchJudge/searchJudge.component.ts
+++ src/app/components/searchJudge/searchJudge.component.ts
@@ -1,6 +1,15 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { SearchBar } from "tns-core-modules/ui/search-bar";
 import { ObservableArray } from "tns-core-modules/data/observable-array";
+import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
+import { NavigationExtras } from "@angular/router";
+import { Component, ChangeDetectionStrategy, AfterViewInit } from "@angular/core";
+import { SearchBar } from "tns-core-modules/ui/search-bar";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+import {RadSideDrawerComponent} from "nativescript-ui-sidedrawer/angular";
+import {RadSideDrawer} from "nativescript-ui-sidedrawer";
+import * as ApplicationSettings from "tns-core-modules/application-settings";
 
 class DataItem {
     constructor(public name: string) { }
@@ -11,21 +20,36 @@ class DataItem {
     templateUrl: "./searchJudge.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchJudgeComponent {
+export class SearchJudgeComponent implements OnInit, AfterViewInit {
+
+    @ViewChild(RadSideDrawerComponent)
+    public drawerComponent: RadSideDrawerComponent;
+    private drawer: RadSideDrawer;
+
+    constructor(private router: RouterExtensions) {}
+
+    ngOnInit(): void { }
+
+    goHome() {
+        this.router.navigate(["/home"], {clearHistory: true})
+    }
+
     private arrayItems: Array<DataItem> = [];
     public myItems: ObservableArray<DataItem> = new ObservableArray<DataItem>();
 
-    constructor() {
-        this.arrayItems.push(new DataItem("Judge_1"));
-        this.arrayItems.push(new DataItem("Judge_2"));
-        this.arrayItems.push(new DataItem("Judge_3"));
-        this.arrayItems.push(new DataItem("Judge_4"));
-        this.arrayItems.push(new DataItem("Judge_5"));
-        this.arrayItems.push(new DataItem("Judge_6"));
-        this.arrayItems.push(new DataItem("Judge_7"));
+                      // TO DO
 
-        this.myItems = new ObservableArray<DataItem>(this.arrayItems);
-    }
+    // constructor() {
+    //     this.arrayItems.push(new DataItem("Judge_1"));
+    //     this.arrayItems.push(new DataItem("Judge_2"));
+    //     this.arrayItems.push(new DataItem("Judge_3"));
+    //     this.arrayItems.push(new DataItem("Judge_4"));
+    //     this.arrayItems.push(new DataItem("Judge_5"));
+    //     this.arrayItems.push(new DataItem("Judge_6"));
+    //     this.arrayItems.push(new DataItem("Judge_7"));
+
+    //     this.myItems = new ObservableArray<DataItem>(this.arrayItems);
+    // }
 
     public onSubmit(args) {
         let searchBar = <SearchBar>args.object;
@@ -52,4 +76,44 @@ export class SearchJudgeComponent {
         });
     }
 
+    public goBack() {
+        this.router.navigate(["/home", false], {clearHistory: true})
+    }
+
+    ngAfterViewInit(): void {
+        this.drawer = this.drawerComponent.sideDrawer;
+    }
+
+    public openDrawer() {
+        this.drawer.showDrawer();
+    }
+
+    public onCloseDrawerTap() {
+        this.drawer.closeDrawer();
+    }
+
+    public navigateBeerDetails(name) {
+        this.router.navigate(["/beerDetails", name]);
+    }
+
+    public navigateSearchBeer() {
+        this.router.navigate(["/searchBeer"]);
+    }
+
+    public navigateSearchJudge() {
+        this.router.navigate(["/searchJudge"]);
+    }
+
+    public navigateHome() {
+        this.router.navigate(["/home"]);
+    }
+
+    public logout() {
+        ApplicationSettings.remove("authenticated");
+        this.router.navigate(["/selectLoginType"], { clearHistory: true });
+    }
+
+    public navigateJudgeBeer() {
+        this.router.navigate(["/judgeBeer"]);
+    }
 }
